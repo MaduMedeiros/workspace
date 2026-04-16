e9380ebb68a7e1bb39908b0f15601fbab7578335
Maria Eduarda Medeiros <dudamellemos15@gmail.com>
2026-04-16 21:09:02 +0000

    adicionar funções bibliotecas

---
+++ funções_bibliotecas.py
@@ -0,0 +1,10 @@
+def minha_funcao():
+    nome = "Madu"
+    print(f"essa é a função da {nome}")
+
+def somar(n1, n2):
+    resultado = n1 + n2
+    print(resultado)
+
+minha_funcao()
+somar(20, 20)
