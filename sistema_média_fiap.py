8c94e0d80b868b9079774151f88ac30cf15dfd64
MaduMedeiros <dudamellemos15@gmail.com>
2026-04-19 14:58:40 -0300

    sistema de cálculo de notascom diferentes pesos

---
+++ sistema_média_fiap.py
@@ -0,0 +1,29 @@
+cp1 = float(input("Insira em um número de 0 a 10 a nota do primeiro Checkpoint:"))
+cp2 = float(input("Insira em um número de 0 a 10 a nota do segundo Checkpoint:")) 
+cp3 = float(input("Insira em um número de 0 a 10 a nota do terceiro Checkpoint:"))
+sprint1 = float(input("Insira em um número de 0 a 10 a nota da primeira Sprint:"))
+sprint2 = float(input("Insira em um número de 0 a 10 a nota da segunda Sprint:"))
+gs = float(input("Insira em um número de 0 á 10 a nota da Global Solution:"))
+
+all_cp = [cp1, cp2, cp3]
+
+def  calc_media1(all_cp, sprint1, sprint2):
+   menor_cp = min(all_cp)
+   result1 = (sum(all_cp) - menor_cp + sprint1 + sprint2) / 4
+   return(result1)
+
+media_cps = calc_media1(all_cp, sprint1, sprint2)
+
+def calc_media2(media_cps, gs):
+   result2 = (media_cps * 0.4 + gs * 0.6)
+   return(result2)
+    
+media_semestre = calc_media2(media_cps, gs)
+print(f"Nota deste semestre sem a aplicação do peso: {media_semestre}")
+
+def media_peso(media_semestre):
+   result3 = (media_semestre * 0.4)
+   return(result3)
+
+media = media_peso(media_semestre)
+print(f"Nota final deste semestre com a aplicação do peso: {media:.1f}")
