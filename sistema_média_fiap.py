116ca6a825147113c21706c6f49742d58e233649
MaduMedeiros <dudamellemos15@gmail.com>
2026-04-22 12:04:07 -0300

    comentado

--- sistema_média_fiap.py
+++ sistema_média_fiap.py
@@ -7,13 +7,16 @@ gs = float(input("Insira em um número de 0 á 10 a nota da Global Solution:"))
 
 all_cp = [cp1, cp2, cp3]
 
+#calculando a média das cps e sprints
 def  calc_media1(all_cp, sprint1, sprint2):
    menor_cp = min(all_cp)
    result1 = (sum(all_cp) - menor_cp + sprint1 + sprint2) / 4
    return(result1)
 
-media_cps = calc_media1(all_cp, sprint1, sprint2)
+#pra agilizar
+media_cps = calc_media1(all_cp, sprint1, sprint2) 
 
+#calculando média do semestre
 def calc_media2(media_cps, gs):
    result2 = (media_cps * 0.4 + gs * 0.6)
    return(result2)
@@ -21,6 +24,7 @@ def calc_media2(media_cps, gs):
 media_semestre = calc_media2(media_cps, gs)
 print(f"Nota deste semestre sem a aplicação do peso: {media_semestre:.1f}")
 
+#calculando o peso do semestre
 def media_peso(media_semestre):
    result3 = (media_semestre * 0.4)
    return(result3)
